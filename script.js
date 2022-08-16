8df7ed4b5f558372d5959989846c187dd31b7ce2
Chris9694 <69814180+tv4Interactivo@users.noreply.github.com>
2022-08-16 14:04:00 -0500

    version 1.0.5

--- script.js
+++ script.js
@@ -47,6 +47,16 @@ function changeLang() {
   const eng = document.getElementById("text-eng");
   const esp2 = document.getElementById("text-esp2");
   const eng2 = document.getElementById("text-eng2");
+  const esp3 = document.getElementById("text-esp3");
+  const eng3 = document.getElementById("text-eng3");
+  const esp4 = document.getElementById("text-esp4");
+  const eng4 = document.getElementById("text-eng4");
+  const esp5 = document.getElementById("text-esp5");
+  const eng5 = document.getElementById("text-eng5");
+  const esp6 = document.getElementById("text-esp6");
+  const eng6 = document.getElementById("text-eng6");
+  const esp7 = document.getElementById("text-esp7");
+  const eng7 = document.getElementById("text-eng7");
 
   if (esp.style.display === "flex") {
     esp.style.display = "none";
@@ -63,6 +73,49 @@ function changeLang() {
     esp2.style.display = "flex";
     eng2.style.display = "none";
   }
+
+  if (esp3.style.display === "flex") {
+    esp3.style.display = "none";
+    eng3.style.display = "flex";
+  } else {
+    esp3.style.display = "flex";
+    eng3.style.display = "none";
+  }
+
+  if (esp4.style.display === "block") {
+    esp4.style.display = "none";
+    eng4.style.display = "block";
+  } else {
+    esp4.style.display = "block";
+    eng4.style.display = "none";
+  }
+
+  if (esp5.style.display === "block") {
+    esp5.style.display = "none";
+    eng5.style.display = "block";
+  } else {
+    esp5.style.display = "block";
+    eng5.style.display = "none";
+  }
+
+  if (esp6.style.display === "block") {
+    esp6.style.display = "none";
+    eng6.style.display = "block";
+  } else {
+    esp6.style.display = "block";
+    eng6.style.display = "none";
+  }
+
+  if (esp7.style.display === "block") {
+    esp7.style.display = "none";
+    eng7.style.display = "block";
+  } else {
+    esp7.style.display = "block";
+    eng7.style.display = "none";
+  }
+
+
+
 
   toggle.style.display = "none";
 }
